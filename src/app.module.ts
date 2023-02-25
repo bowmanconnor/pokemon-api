ff42e646c758e2ae4bfb3e290151f19a383aac48
Connor <connorbowman4@yahoo.com>
2023-02-25 15:19:14 -0500

    Implement mongoDB connection. Create config module for secure connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, MongooseModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => configService.getMongoConfig(),
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
