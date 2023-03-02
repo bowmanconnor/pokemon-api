b0f6bdc890135ebab35e1394a4458e2b9b41232e
Connor <connorbowman4@yahoo.com>
2023-03-01 23:24:04 -0500

    Fix typo

---
+++ test/infrastructure/scripts/connect-db.spec.ts
@@ -0,0 +1,49 @@
+
+import mongoose, { Model } from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { connectDB, disconnectDB } from '../../../src/infrastructure/scripts/connect-db';
+
+// Ignore console.log lines in seed, purge, connectDB, disconnectDB
+console.log = function () { };
+
+describe('seed-db', () => {
+    let mongo = null
+    let uri: string
+
+    beforeAll(async () => {
+        // Create mock mongoDB database to not interfere with production database
+        mongo = await MongoMemoryServer.create();
+        uri = mongo.getUri();
+    });
+
+    afterAll(async () => {
+        if (mongo) {
+            await mongo.stop();
+        }
+    });
+
+    it('should connect to a mongoDB database', async () => {
+        // except the connection to be disconnected initialy
+        expect(mongoose.connection.readyState).toBe(0);
+
+        await connectDB(uri);
+
+        // expect the connection to be connected now
+        expect(mongoose.connection.readyState).toBe(1);
+
+    });
+
+    it('should disconnect to a mongoDB database', async () => {
+        // expect the connection to be connected still
+        expect(mongoose.connection.readyState).toBe(1);
+
+        await disconnectDB();
+
+        // expect the connection to be disconnected finally
+        expect(mongoose.connection.readyState).toBe(0);
+
+    });
+
+
+});
+
